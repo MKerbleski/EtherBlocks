d2515597037c8220d398947575ce5def924f5225
mkerbleski <mkerbles@gmail.com>
2019-09-21 08:22:08 -0400

    change Title

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios')
+// const web3 = require('web3')
+
+// web3.eth.getBlock('genisis', true, (err, result) => {
+// 	if(err){console.log(err)}
+// 	if(result){console.log(result)}
+// }))
 
 router.get('/sample', (req, res, next) => {
 })
